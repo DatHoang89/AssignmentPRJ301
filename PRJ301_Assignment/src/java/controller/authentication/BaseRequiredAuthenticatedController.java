a17c4a2d11dee31197150216022fcdbbba625dbf
DatHoang89 <datvip1938@gmail.com>
2023-03-02 08:33:53 +0700

    add login controller

---
+++ PRJ301_Assignment/src/java/controller/authentication/BaseRequiredAuthenticatedController.java
@@ -0,0 +1,83 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller.authentication;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import model.User;
+
+/**
+ *
+ * @author LEGION
+ */
+public abstract class BaseRequiredAuthenticatedController extends HttpServlet {
+
+    private boolean isAuthenticated(HttpServletRequest request) {
+        return request.getSession().getAttribute("user") != null;
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        if(isAuthenticated(request))
+        {
+            //do business
+            doGet(request, response, (User)request.getSession().getAttribute("user"));
+        }
+        else
+        {
+            response.getWriter().println("Access denied!");
+        }
+    }
+    protected abstract void doGet(HttpServletRequest request, HttpServletResponse response,User user)
+            throws ServletException, IOException;
+    protected abstract void doPost(HttpServletRequest request, HttpServletResponse response,User user)
+            throws ServletException, IOException;
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        if(isAuthenticated(request))
+        {
+            //do business
+            doPost(request, response, (User)request.getSession().getAttribute("user"));
+        }
+        else
+        {
+            response.getWriter().println("Access denied!");
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
