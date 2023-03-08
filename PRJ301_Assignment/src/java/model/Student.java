0c0936ab76c734cfb5114b9e7385e7d4daedb664
DatHoang89 <datvip1938@gmail.com>
2023-03-08 11:33:08 +0700

    update model package

---
+++ PRJ301_Assignment/src/java/model/Student.java
@@ -0,0 +1,89 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.sql.Date;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author LEGION
+ */
+public class Student {
+    private int stdid;
+    private String stdname;
+    private boolean gender;
+    private Date dob;
+    private String stdphone;
+    private String stdemail;
+    private ArrayList<Group> groups = new ArrayList<>();
+    private ArrayList<Attendance> attendances = new ArrayList<>();
+
+    public int getStdid() {
+        return stdid;
+    }
+
+    public void setStdid(int stdid) {
+        this.stdid = stdid;
+    }
+
+    public String getStdname() {
+        return stdname;
+    }
+
+    public void setStdname(String stdname) {
+        this.stdname = stdname;
+    }
+
+    public boolean isGender() {
+        return gender;
+    }
+
+    public void setGender(boolean gender) {
+        this.gender = gender;
+    }
+
+    public Date getDob() {
+        return dob;
+    }
+
+    public void setDob(Date dob) {
+        this.dob = dob;
+    }
+
+    public String getStdphone() {
+        return stdphone;
+    }
+
+    public void setStdphone(String stdphone) {
+        this.stdphone = stdphone;
+    }
+
+    public String getStdemail() {
+        return stdemail;
+    }
+
+    public void setStdemail(String stdemail) {
+        this.stdemail = stdemail;
+    }
+
+    public ArrayList<Group> getGroups() {
+        return groups;
+    }
+
+    public void setGroups(ArrayList<Group> groups) {
+        this.groups = groups;
+    }
+
+    public ArrayList<Attendance> getAttendances() {
+        return attendances;
+    }
+
+    public void setAttendances(ArrayList<Attendance> attendances) {
+        this.attendances = attendances;
+    }
+    
+    
+}
